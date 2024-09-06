8f5e9cd278c6fe0a076d857ea1e73886246bbbda
riohik <riohik@gmail.com>
2024-09-06 21:05:34 +0900

    first commit

---
+++ src/app/components/about.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Image from "next/image";
+import Template from './templates/template';
+
+export default function About() {
+    return (
+        <Template>
+            <div className="flex flex-row items-center">
+                <div className="flex flex-col items-center justify-center">
+                    <Image src="/yamo_logo.png" alt="Yamo ロゴ" width={200} height={100} />
+                    <p className="text-center">
+                        ってどういう意味？
+                    </p>
+                </div>
+                <div className="flex flex-col justify-center items-start gap-5">
+                    <p className="text-left">
+                        会社名の「やも」は、漢字で書くと「八面 / 八方」。 八方の方面、転じて、「あらゆる方向」と言う意味があります。
+                    </p>
+                    <p className="text-left">
+                        あらゆる方々から常に必要とされるような存在であること、 ご提案に対して、多角的な視点で、柔軟性をもって 映像を制作していくことを大切にしたいという 思いが込められています。
+                    </p>
+                    <p className="text-left">
+                        また、少し古い言葉になりますが、
+                        和歌などの語尾に「やも」と用いることで、 「仮説」や「詠嘆」の意味をもつことがあります。
+                    </p>
+                    <p className="text-left">
+                        常に「〇〇かなあ」と疑問符を持ち、吐露し合うこと。 心で思ったことを伝え合う関係性があってこそ 良いチーム、良い映像を制作できると考えています。
+                    </p>
+                    <p className="text-left">
+                        この社名に込めた言葉の意味を大切に、 映像で八方を虜にできるような「やも」を目指して、 精進し続けていきます。
+                    </p>
+                </div>
+            </div>
+        </Template>
+    )
+}
