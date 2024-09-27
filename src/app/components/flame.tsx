ec629d30b8750c605e07922a5ac691b742b292ae
riohik <riohik@gmail.com>
2024-09-28 00:12:52 +0900

    あとレスポンシブと画像並べ対応

--- src/app/components/flame.tsx
+++ src/app/components/flame.tsx
@@ -2,21 +2,14 @@
 import { useEffect, useState } from 'react';
 import { YouTubeEmbed } from "@next/third-parties/google"
 import Template from './templates/template';
+import { useMediaQuery } from 'react-responsive'
 
 export default function Flame() {
-    const [windowWidth, setWindowWidth] = useState(0);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const handleResize = () => setWindowWidth(window.innerWidth);
-            window.addEventListener('resize', handleResize);
-            return () => window.removeEventListener('resize', handleResize);
-        }
-    }, []);
+    const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
     return (
         <Template>
             <div className="flex items-center justify-center">
-                <YouTubeEmbed videoid={process.env.NEXT_PUBLIC_VIDEO_ID || ''} width={window.innerWidth <= 767 ? 400 : 700} />
+                <YouTubeEmbed videoid={process.env.NEXT_PUBLIC_VIDEO_ID || ''} width={isMobile ? 400 : 700} />
             </div>
         </Template>
     )
--- src/app/components/top.tsx
+++ src/app/components/top.tsx
@@ -2,22 +2,15 @@
 import { useEffect, useState } from 'react';
 import Image from "next/image";
 import Template from './templates/template';
+import { useMediaQuery } from 'react-responsive'
 
 export default function Top() {
-    const [windowWidth, setWindowWidth] = useState(0);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const handleResize = () => setWindowWidth(window.innerWidth);
-            window.addEventListener('resize', handleResize);
-            return () => window.removeEventListener('resize', handleResize);
-        }
-    }, []);
+    const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
     return (
         <Template isBlueBack={true}>
             <div className="flex flex-row items-center">
                 <span className={`mr-2 md:pt-10 lg:pt-20 font-normal text-5xl`}>株式会社</span>
-                <Image src="/yamo_logo_gold.png" alt="Yamo ロゴ" width={window.innerWidth <= 767 ? 300 : 500} height={300} />
+                <Image src="/yamo_logo_gold.png" alt="Yamo ロゴ" width={isMobile ? 300 : 500} height={300} />
             </div>
         </Template>
     )
