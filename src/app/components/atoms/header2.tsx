7c787c85f599a3e4ee020412df56c1957121b19b
riohik <riohik@gmail.com>
2024-10-04 15:30:18 +0900

    文字サイズ調整

--- src/app/components/atoms/header2.tsx
+++ src/app/components/atoms/header2.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 type Props = {
+    is2Xl? :boolean
     children: React.ReactNode;
 };
 
-const Header2: React.FC<Props> = ({ children }) => {
+const Header2: React.FC<Props> = ({ is2Xl = false, children }) => {
 
     return (
-        <h2 className="text-4xl lg:text-5xl font-bold">{children}</h2>
+        <h2 className={`font-bold ${is2Xl ? 'text-2xl' : 'text-3xl'} lg:text-5xl`}>{children}</h2>
     );
 };
 export default Header2;
