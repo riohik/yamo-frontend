659f447a35ce0f0d1c750b3d7016bae8b93645d9
riohik <riohik@gmail.com>
2024-10-09 13:21:54 +0900

    改行

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -7,25 +7,25 @@ export default function About() {
             <div className="flex flex-row items-center gap-2 p-1">
                 <div className="flex flex-col items-center justify-center w-2/5">
                     <Image src="/yamo_logo_blue.jpg" alt="Yamo ロゴ" width={400} height={100} />
-                    <p className="text-center">
+                    <p className="text-center text-xs lg:text-base">
                         ってどういう意味？
                     </p>
                 </div>
                 <div className="flex flex-col justify-center items-start gap-5 w-3/5 lg:pr-20 text-[10px] lg:text-lg">
                     <p className="text-left">
-                        会社名の「やも」は、漢字で書くと「八面 / 八方」。<br className="lg:block hidden" /> 八方の方面、<br className="lg:hidden block" />転じて、「あらゆる方向」と言う意味があります。
+                        会社名の「やも」は、<br className="lg:hidden block" />漢字で書くと「八面 / 八方」。<br className="block" /> 八方の方面、転じて、<br className="lg:hidden block" />「あらゆる方向」<br className="lg:hidden block" />と言う意味があります。
                     </p>
                     <p className="text-left">
-                        あらゆる方々から常に必要とされるような存在であること、 <br className="block" />ご提案に対して、多角的な視点で、柔軟性をもって<br className="block" />映像を制作していくことを大切にしたいという<br className="lg:block hidden" />思いが込められています。
+                        あらゆる方々から常に<br className="lg:hidden block" />必要とされるような<br className="lg:hidden block" />存在であること、 <br className="block" />ご提案に対して、<br className="lg:hidden block" />多角的な視点で、<br className="lg:hidden block" />柔軟性をもって<br className="hidden lg:block" />映像を制作していくことを大切にしたいという<br className="block" />思いが込められています。
                     </p>
                     <p className="text-left">
-                        また、少し古い言葉になりますが、<br className="block" />和歌などの語尾に<br className="lg:hidden block" />「やも」と用いることで、<br className="block" />「仮説」や「詠嘆」の意味をもつことがあります。
+                        また、<br className="lg:hidden block" />少し古い言葉になりますが、<br className="block" />和歌などの語尾に<br className="lg:hidden block" />「やも」と用いることで、<br className="block" />「仮説」や「詠嘆」の意味をもつことがあります。
                     </p>
                     <p className="text-left">
-                        常に「〇〇かなあ」と疑問符を持ち、吐露し合うこと。<br className="block" /> 心で思ったことを伝え合う関係性があってこそ<br className="block" />良いチーム、良い映像を制作できると考えています。
+                        常に「〇〇かなあ」と<br className="lg:hidden block" />疑問符を持ち、<br className="lg:hidden block" />吐露し合うこと。<br className="block" /> 心で思ったことを伝え合う<br className="lg:hidden block" />関係性があってこそ<br className="block" />良いチーム、良い映像を制作できると考えています。
                     </p>
                     <p className="text-left">
-                        この社名に込めた言葉の意味を大切に、<br className="block" /> 映像で八方を虜にできるような「やも」を目指して、<br className="block" /> 精進し続けていきます。
+                        この社名に込めた<br className="lg:hidden block" />言葉の意味を大切に、<br className="block" /> 映像で八方を虜にできるような<br className="lg:hidden block" />「やも」を目指して、<br className="block" /> 精進し続けていきます。
                     </p>
                 </div>
             </div>
--- src/app/components/company.tsx
+++ src/app/components/company.tsx
@@ -7,19 +7,19 @@ export default function Company() {
             <div className="flex flex-row items-center gap-2 p-1">
                 <div className="flex flex-col items-center justify-center w-2/5">
                     <Image src="/yamo_logo_white.png" alt="Yamo ロゴ" width={400} height={100} className="filter brightness-300" />
-                    <p className="text-center">
+                    <p className="text-center text-xs lg:text-base">
                         ってどんな会社？
                     </p>
                 </div>
                 <div className="flex flex-col justify-center items-start gap-10 w-3/5 lg:pr-20 text-xs lg:text-xl">
                     <p className="text-left">
-                        お客様がつくりたい映像を制作するために<br className="block" />最適なチーム作りから考える会社です。
+                        お客様がつくりたい映像を制作するために<br className="block" />最適なチーム作りから<br className="lg:hidden block" />考える会社です。
                     </p>
                     <p className="text-left">
-                        今まで映像制作に携わってきた経験を踏まえ、<br className="block" />コストを抑えながら、機動力を持って、<br className="block" />どのようなメンバーで映像を制作することが<br className="block" />最高の結果を導くことができるのか。
+                        今まで映像制作に携わってきた経験を踏まえ、<br className="block" />コストを抑えながら、<br className="lg:hidden block" />機動力を持って、<br className="block" />どのようなメンバーで<br className="lg:hidden block" />映像を制作することが<br className="block" />最高の結果を導くことが<br className="lg:hidden block" />できるのか。
                     </p>
                     <p className="text-left">
-                        お客様のオーダーから、<br className="block" />素早くチームをスタッフィングし、<br className="block" />最高のチームワークで映像を制作します
+                        お客様のオーダーから、<br className="block" />素早くチームを<br className="lg:hidden block" />スタッフィングし、<br className="block" />最高のチームワークで<br className="lg:hidden block" />映像を制作します
                     </p>
                 </div>
             </div>
